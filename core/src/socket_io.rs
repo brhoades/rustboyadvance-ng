a9f5e5cc294012c9457af864393b6d2b59d432ad
Billy J Rhoades II <me@brod.es>
2023-05-29 16:07:55 -0700

    data now gets smashed over a file socket!

--- core/src/socket_io.rs
+++ core/src/socket_io.rs
@@ -1,7 +1,9 @@
 use std::{
     env, error, fmt, fs,
+    io::Write,
     os::unix::net::{UnixListener, UnixStream},
     path::{Path, PathBuf},
+    time,
 };
 
 use serde::{Deserialize, Serialize};
@@ -24,7 +26,14 @@ pub(crate) struct SocketIO {
     rx: UnixListener,
     // print errors out every Nth error to prevent spam
     errcnt: usize,
+    i: usize,
 }
+
+// we notify once every 30 event refreshes
+const ERR_COUNT_NOTIFY: usize = 30;
+
+// when modulus of i and v is 0, we send
+const SEND_FREQ: usize = 50;
 
 #[derive(Serialize, Deserialize, Default)]
 pub(crate) struct GameState<'a, 'b> {
@@ -42,36 +51,65 @@ impl SocketIO {
         let tx_fn = Path::new(tx_fn);
         let rx_fn = Path::new(rx_fn);
 
-        if tx_fn.exists() {
-            fs::remove_file(&tx_fn).unwrap();
+        if rx_fn.exists() {
+            fs::remove_file(&rx_fn).unwrap();
         }
         let rx = UnixListener::bind(&rx_fn).unwrap();
-        println!("now listening for IO on {}", tx_fn.to_string_lossy());
+        println!("now listening for IO on {}", rx_fn.to_string_lossy());
 
         if let Some(mut sched) = sched {
             sched.schedule_at(EventType::SocketIO, CYCLES_FULL_REFRESH * 5);
         }
 
-        let tx = match UnixStream::connect(&tx_fn) {
-            Ok(ln) => TxSocketState::Open(ln),
-            Err(e) => {
-                let err = e.to_string();
-                println!("failed to open tx socket: {}", &err);
-                TxSocketState::Retrying(err)
-            }
-        };
-
-        println!("INITIALIZED");
-        SocketIO {
-            tx,
+        let mut sock_io = SocketIO {
+            tx: TxSocketState::Retrying("first run".to_string()),
             tx_fn: tx_fn.to_path_buf(),
             rx,
             errcnt: 0,
-        }
+            i: 0,
+        };
+
+        let _ = sock_io.try_open_tx();
+        println!("SocketIO initialized");
+
+        sock_io
     }
 
     pub(crate) fn on_event(&mut self, state: GameState) -> Option<FutureEvent> {
-        println!("EVENT GET: {}", state.time);
+        self.i += 1;
+        let can_err = self.errcnt % ERR_COUNT_NOTIFY == 0;
+
+        if self.try_open_tx().is_err() {
+            self.errcnt += 1;
+        }
+
+        match &mut self.tx {
+            TxSocketState::Retrying(err) if can_err => {
+                println!(
+                    "failed to open tx socket {}: {}",
+                    self.tx_fn.to_string_lossy(),
+                    err
+                );
+            }
+            TxSocketState::Open(sock) if self.i % SEND_FREQ == 0 => {
+                match bincode::serialize(&state) {
+                    Err(e) => println!("failed serialize state: {}", e),
+                    Ok(s) => {
+                        if let Err(e) = sock.write_all(&s) {
+                            self.errcnt += 1;
+                            if can_err {
+                                println!("failed to write state to socket: {}", e);
+                                self.tx = TxSocketState::Retrying(e.to_string());
+                            }
+                        } else {
+                            self.errcnt = 0;
+                        }
+                    }
+                };
+            }
+            _ => (),
+        }
+
         Some((EventType::SocketIO, CYCLES_FULL_REFRESH))
     }
 
@@ -82,12 +120,13 @@ impl SocketIO {
             Open(_) => Ok(()),
             Retrying(_) => {
                 self.tx = match UnixStream::connect(&self.tx_fn) {
-                    Ok(ln) => TxSocketState::Open(ln),
-                    Err(e) => {
-                        let err = e.to_string();
-                        println!("failed to open tx socket: {}", &err);
-                        TxSocketState::Retrying(err)
+                    Ok(ln) => {
+                        ln.set_nonblocking(true).unwrap();
+                        ln.set_write_timeout(Some(time::Duration::from_micros(100)))
+                            .unwrap();
+                        TxSocketState::Open(ln)
                     }
+                    Err(e) => TxSocketState::Retrying(e.to_string()),
                 };
 
                 match &self.tx {
